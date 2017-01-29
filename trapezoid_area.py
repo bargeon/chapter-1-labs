09c4e2890cc60f747e38f739ae9e5ac15f7584b7
bargeon <barajaselian47@gmail.com>
2017-01-29 13:41:34 -0800

    -

--- trapezoid_area.py
+++ trapezoid_area.py
@@ -4,10 +4,10 @@ print("This program calculates the area of the trapezoid")
 # gathering user input
 trap_height = float(input("Enter the height of the trapezoid: "))
 first_base = float(input("Enter the length of bottom base: "))
-second_base = float(input("Enter the length of the top side: "))
+top_side = float(input("Enter the length of the top side: "))
 
 # calculating area
-trap_area = 1 / 2 * (first_base + second_base) * trap_height
+trap_area = 1 / 2 * (first_base + top_side) * trap_height
 
 # printing area
 print("The area of the trapezoid is:", trap_area)
